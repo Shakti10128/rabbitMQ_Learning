4e38d62fd991aba47458c584c335fcbd0339ad05
shakti <kumarshakti2017@gmail.com>
2025-08-01 11:08:46 +0530

    minor changes in the comments of producer file

--- HeaderExchange/producer.js
+++ HeaderExchange/producer.js
@@ -27,11 +27,14 @@ async function sendNotification(headers,message){
     }
 }
 
-// all -> & condition
-// any -> | condition
+// Using x-match: "all" => Message will go only to queues that match **all** header keys/values
+sendNotification({ "x-match": "all", "notification-type": "new_video", "content-type": "video" },"New Video Uploaded");
 
-// sendNotification(headers,message)
-sendNotification({"x-match":"all","notification-type":"new_video","content-type":"video"},"New Video Uploaded")
-sendNotification({"x-match":"all","notification-type":"live_stream","content-type":"gaming"},"gaming live stream started")
-sendNotification({"x-match":"any","notification-type-comment":"comment","content-type":"vlog"},"New comment on your blog")
-sendNotification({"x-match":"any","notification-type-like":"like","content-type":"vlog"},"New like on your blog")
+// Using x-match: "all" => All headers must match
+sendNotification({ "x-match": "all", "notification-type": "live_stream", "content-type": "gaming" },"Gaming live stream started");
+
+// Using x-match: "any" => Message will go to queues that match **any one** of the headers
+sendNotification({ "x-match": "any", "notification-type-comment": "comment", "content-type": "vlog" },"New comment on your blog");
+
+// Same with "like"
+sendNotification({ "x-match": "any", "notification-type-like": "like", "content-type": "vlog" },"New like on your blog");
